c1f52336f72a3e0469664d1e9fcfdaa6dbf7532a
Kyanna <kyanna@kyannashanice.ca>
2023-07-27 22:56:46 -0700

    photo page updates
    not working -> to merge with embedded video
    
    updated div class section -> section tag
    
    add photos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,41 @@
-// makes the parallax elements
-function parallaxIt() {
-    // create variables
-    var $fwindow = $(window);
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  
-    var $contents = [];
-    var $backgrounds = [];
-  
-    // for each of content parallax element
-    $('[data-type="content"]').each(function(index, e) {
-      var $contentObj = $(this);
-  
-      $contentObj.__speed = ($contentObj.data('speed') || 1);
-      $contentObj.__fgOffset = $contentObj.offset().top;
-      $contents.push($contentObj);
+var modal = document.getElementById("modal");
+    var closeButton = document.getElementsByClassName("close")[0];
+
+    var links = document.getElementsByClassName("modal-link");
+    var modalImage = document.getElementById("modal-image");
+
+    function openModal(imageSrc) {
+      modal.style.display = "block";
+      modalImage.src = imageSrc;
+    }
+
+    function closeModal() {
+      modal.style.display = "none";
+    }
+
+    Array.from(links).forEach(function(link) {
+      link.addEventListener("click", function(event) {
+        event.preventDefault();
+        openModal(this.href);
+      });
     });
+
+    closeButton.addEventListener("click", closeModal);
+
+    window.addEventListener("click", function(event) {
+      if (event.target == modal) {
+        closeModal();
+      }
+    });
+
+    // Get the modal
+var modal = document.getElementById("modal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("photo_img");
+var modalImg = document.getElementById("modal-img");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
